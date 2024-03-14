e9c6bd092c18df5b7693d00f24f321e6a720c505
nabeel-ahsan <itsme007nabeel@gmail.com>
2024-03-14 22:56:02 +0530

    truthy and falsy

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,43 +1,45 @@
-const userEmail = []
+const userEmail = []  // Declares a constant array `userEmail` but initializes it as empty
 
-if (userEmail) {
-    console.log("Got user email");
+if (userEmail) {  // Checks if `userEmail` is truthy (not null, undefined, empty string, 0, etc.)
+    console.log("Got user email");  // This won't be executed because `userEmail` is empty (falsy)
 } else {
-    console.log("Don't have user email");
+    console.log("Don't have user email");  // This will be executed because `userEmail` is empty
 }
 
-// falsy values
+// Falsy values are:
+// false, 0, -0, BigInt(0n), "", null, undefined, NaN
 
-// false, 0, -0, BigInt 0n, "", null, undefined, NaN
-
-//truthy values
+// Truthy values are (almost) everything else:
 // "0", 'false', " ", [], {}, function(){}
 
-// if (userEmail.length === 0) {
+// if (userEmail.length === 0) {  // More explicit check for empty array
 //     console.log("Array is empty");
 // }
 
-const emptyObj = {}
+const emptyObj = {}  // Declares a constant object `emptyObj` and initializes it as empty
 
-if (Object.keys(emptyObj).length === 0) {
-    console.log("Object is empty");
+if (Object.keys(emptyObj).length === 0) {  // Checks if the object's key count is 0 (empty)
+    console.log("Object is empty");  // This will be executed because `emptyObj` has no keys
 }
 
-// Nullish Coalescing Operator (??): null undefined
+// Nullish Coalescing Operator (??)
 
 let val1;
-// val1 = 5 ?? 10
-// val1 = null ?? 10
-// val1 = undefined ?? 15
-val1 = null ?? 10 ?? 20
+// val1 = 5 ?? 10  // Assigns 5 (truthy) to val1
+// val1 = null ?? 10  // Assigns 10 (because null is nullish)
+// val1 = undefined ?? 15  // Assigns 15 (because undefined is nullish)
+val1 = null ?? 10 ?? 20;  // Assigns 10 (null is nullish, 10 is truthy)
 
-
-
-console.log(val1);
+console.log(val1);  // This will print 10
 
 // Terniary Operator
 
 // condition ? true : false
 
-const iceTeaPrice = 100
-iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+const iceTeaPrice = 100;
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");  // This will print "more than 80"
+
+// Important points:
+// * Empty arrays and objects are considered falsy values in JavaScript.
+// * The nullish coalescing operator (??) checks if a value is nullish (null or undefined) and if so, assigns the value after the ?? operator.
+// * The ternary operator provides a shorthand way to write if-else statements.
