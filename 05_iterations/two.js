2566242efb408cfd08cc9c07b08f87ad399632f5
nabeel-ahsan <itsme007nabeel@gmail.com>
2024-04-23 14:03:00 +0530

    added notes

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,21 +1,37 @@
+let index = 0; // Initializing a variable 'index' with value 0
 
-let index = 0
+// Using a while loop to iterate as long as 'index' is less than or equal to 10
+// Note: This loop is commented out
 // while (index <= 10) {
-//     console.log(`Value of index is ${index}`);
-//     index = index + 2
+//     console.log(`Value of index is ${index}`); // Logging the value of 'index'
+//     index = index + 2; // Incrementing 'index' by 2
 // }
 
-let myArray = ['flash', "batman", "superman"]
+let myArray = ['flash', "batman", "superman"]; // Creating an array of superhero names
 
-let arr = 0
+let arr = 0; // Initializing a variable 'arr' with value 0
+
+// Using a while loop to iterate through the elements of 'myArray'
 while (arr < myArray.length) {
-    //console.log(`Value is ${myArray[arr]}`);
-    arr = arr + 1
+    // Logging the value of each element in 'myArray'
+    // Note: This loop is commented out
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1; // Incrementing 'arr' to move to the next element in 'myArray'
 }
 
-let score = 11
+let score = 11; // Initializing a variable 'score' with value 11
 
+// Using a do-while loop to execute a block of code at least once, then continue while a condition is true
 do {
-    console.log(`Score is ${score}`);
-    score++
-} while (score <= 10);
+    console.log(`Score is ${score}`); // Logging the value of 'score'
+    score++; // Incrementing 'score' by 1
+} while (score <= 10); // Continuing the loop as long as 'score' is less than or equal to 10
+
+
+// The while loop executes a block of code repeatedly while a specified condition is true.
+
+// The do-while loop is similar to the while loop, but it always executes its block of code at least once before checking the condition.
+
+// It's important to ensure that the condition in a loop is eventually met to prevent infinite loops.
+
+// Incrementing or decrementing loop counters within the loop ensures proper iteration over arrays or sequences.
