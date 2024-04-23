5fe8c59eaa60cc17bbaad4994c35af627a65a157
nabeel-ahsan <itsme007nabeel@gmail.com>
2024-04-23 14:04:07 +0530

    added notes

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,12 +1,15 @@
 const myNums = [1, 2, 3]
 
-// const myTotal = myNums.reduce(function (acc, currval) {
-//     console.log(`acc: ${acc} and currval: ${currval}`);
-//     return acc + currval
-// }, 0)
+// Using the reduce method to sum up all elements in the array.
+// The reduce method takes a function as its first argument.
+// This function takes two parameters: 'acc' (accumulator) and 'curr' (current value).
+// The 'acc' parameter stores the accumulated value, while 'curr' represents the current element being processed.
+// In this example, the arrow function adds the current value to the accumulator.
+// The '0' at the end is the initial value of the accumulator.
+// If omitted, the initial value defaults to the first element of the array.
+const myTotal = myNums.reduce((acc, curr) => acc + curr, 0)
 
-const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
-
+// Logging the total sum of all elements in the array.
 console.log(myTotal);
 
 
@@ -29,6 +32,21 @@ const shoppingCart = [
     },
 ]
 
+// Using the reduce method to calculate the total price of all items in the shopping cart.
+// The arrow function calculates the total by adding each item's price to the accumulator.
+// The '0' at the end is the initial value of the accumulator.
 const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
 
+// Logging the total price to pay for all items in the shopping cart.
 console.log(priceToPay);
+
+
+// The reduce method is used to apply a function to each element of the array, resulting in a single output value.
+
+// The function provided to reduce takes four arguments: accumulator, currentValue, currentIndex, and array.
+
+// In these examples, we are only using accumulator and currentValue.
+
+// The initial value of the accumulator can be provided as the second argument to reduce. If omitted, the first element of the array is used as the initial value.
+
+// reduce is a versatile method that can be used for various purposes, such as summing up values, calculating averages, or transforming data.
